9c82b57c2d18c3a54609790c39f206e3d3c39c0c
sub7789 <74786838+sub7789@users.noreply.github.com>
2021-12-25 22:25:13 -0700

    Got the program to work with matplotlib - currently runs for 10 seconds then displays your data. Also changed the logo to a transparent version

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 # Fax
 Fax is a tool designed by Python to visualize your usage of apps on your Windows machine, similar to Screen Time on iOS or macOS. Fax uses libraries like Matplotlib, psutil, and more. 
 With Fax you are able to get in control of your PC usage.
+
+**Notes**:  
+Will not work with venv or any other virtual environments  
+To get this to work on anything post Python 3.6, do pip install pywin32 and pip install psutil  
+Template for tracking windows on: https://dev.to/tkkhhaarree/track-windows-app-usage-time-using-python-h9h  
--- main.py
+++ main.py
@@ -2,18 +2,36 @@ from win32gui import GetForegroundWindow
 import psutil
 import time
 import win32process
+import matplotlib.pyplot as plt
 
-# to get this to work on anything post Python 3.6, do pip install pywin32 and pip install psutil
 
-process_time = {}
-timestamp = {}
-while True:
-    current_app = psutil.Process(win32process.GetWindowThreadProcessId(GetForegroundWindow())[1]).name().replace(".exe",
-                                                                                                                 "")
-    timestamp[current_app] = int(time.time())
-    time.sleep(1)
-    if current_app not in process_time.keys():
-        process_time[current_app] = 0
-    process_time[current_app] = process_time[current_app] + int(time.time()) - timestamp[current_app]
-    print(process_time)
-# https://dev.to/tkkhhaarree/track-windows-app-usage-time-using-python-h9h
+def windowTracker():
+    process_time = {}
+    timestamp = {}
+    x = 0
+    while True:
+        current_app = psutil.Process(win32process.GetWindowThreadProcessId(GetForegroundWindow())[1]).name().replace(
+            ".exe", "")
+        timestamp[current_app] = int(time.time())
+        time.sleep(1)
+        if current_app not in process_time.keys():
+            process_time[current_app] = 0
+        process_time[current_app] = process_time[current_app] + int(time.time()) - timestamp[current_app]
+        print(process_time)
+        x += 1
+        if (x == 10):  # TODO: change this to a button or a keyboard signal that stops the timer and returns your converted graph or even the timer ending at the end of the day (for now it's set at 10 seconds for testing purposes)
+            return process_time
+
+
+def plotTestData(x):
+    labels, sizes = zip(*x.items())
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
+            shadow=True, startangle=90)
+    ax1.axis('equal')
+    plt.title("Your App Usage")
+    plt.show()
+
+
+x = windowTracker()
+plotTestData(x)
