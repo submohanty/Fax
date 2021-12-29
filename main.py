120595ab8a12576f9c8683b761ca558c78191d61
sub7789 <74786838+sub7789@users.noreply.github.com>
2021-12-28 22:44:09 -0700

    Added a button that starts it and can restart it after the timer goes out, and added qt-material

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
 from win32gui import GetForegroundWindow
+import win32process
 import psutil
 import time
-import win32process
 import matplotlib.pyplot as plt
+from qt_material import apply_stylesheet
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
+
 
 def windowTracker():
     process_time = {}
     timestamp = {}
     x = 0
-    while True:
+    while True: # TODO: add blacklist of apps that shouldn't be tracked, like Explorer, Search, etc.
         current_app = psutil.Process(win32process.GetWindowThreadProcessId(GetForegroundWindow())[1]).name().replace(
             ".exe", "")
         timestamp[current_app] = int(time.time())
@@ -23,8 +25,7 @@ def windowTracker():
         process_time[current_app] = process_time[current_app] + int(time.time()) - timestamp[current_app]
         print(process_time)
         x += 1
-        if (x == 10):  # TODO: change this to a button or a keyboard signal that stops the timer and returns your converted graph or even the timer ending at the end of the day (for now it's set at 10 seconds for testing purposes)
-            #return process_time
+        if (x == 5):
             labels, sizes = zip(*process_time.items())
             fig1, ax1 = plt.subplots()
             ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
@@ -45,27 +46,51 @@ def plotTestData(x):
     plt.show()
 
 
-def main():
+def keyPressEvent(self, e):
+    if e.key() == QtCore.Qt.Key_Escape:
+        self.close()
+    if e.key() == QtCore.Qt.Key_F11:
+        if self.isMaximized():
+            self.showNormal()
+        else:
+            self.showMaximized()
+
+
+def gui():
+    extra = {
+        'font_family': 'Roboto',
+        'font_size': 22,
+        'density_scale': '-1'
+    }
+
     app = QApplication(sys.argv)
     win = QMainWindow()
-    win.setGeometry(400, 400, 400, 300)
+    apply_stylesheet(app, theme='dark_cyan.xml', extra=extra)
     win.setWindowTitle("Fax")
-    # Label
+
+    #
     label = QLabel(win)
     label.setText("Press this button in order to start the screen time tracking")
-    label.setFont(QFont("Consolas", 12))
-    label.move(0, 25)
+    # label.setFont(QFont("Consolas", 48)) # this doesn't work with qt material
+    label.move(700, 200)
     label.setAlignment(Qt.AlignCenter)
     label.setWordWrap(True)
     label.adjustSize()
+
     # Button
     button = QtWidgets.QPushButton(win)
     button.setText("Begin")
-    button.move(150,100)
+    button.setGeometry(0, 0, 100, 100)
+    button.move(885, 400)
     button.clicked.connect(windowTracker)
     #
-    win.show()
+    # TODO: Add a button that stops the timer
+    # TODO: Add a way for the user to collapse the window into the tray
+
+    win.showMaximized()
     sys.exit(app.exec_())
 
-main()
 
+gui()  # TODO: wrap this all into a class
+
+# TODO: Add a SQL database to store the images of Matplotlib over time so that the user can see their trends over time
